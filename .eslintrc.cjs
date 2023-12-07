b8bad79b2df233843bd7ce3c787506e6e2fcf729
SatooRu65536 <ryuto4619@gmail.com>
2023-12-07 17:19:37 +0900

    init: prettier と lint を設定

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,51 +1,48 @@
 /** @type { import("eslint").Linter.FlatConfig } */
 module.exports = {
-	root: true,
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:svelte/recommended',
-		'prettier'
-	],
-	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint',"unused-imports"],
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020,
-		extraFileExtensions: ['.svelte']
-	},
-	env: {
-		browser: true,
-		es2017: true,
-		node: true
-	},
-	overrides: [
-		{
-			files: ['*.svelte'],
-			parser: 'svelte-eslint-parser',
-			parserOptions: {
-				parser: '@typescript-eslint/parser'
-			}
-		}
-	],
-	rules:{
-		"@typescript-eslint/no-unused-vars": "off",
-		"unused-imports/no-unused-imports": "error",
-		"prefer-destructuring": [
-			"error",
-			{
-			  "array": false,
-			  "object": true
-			}
-		  ],
-		  //varを禁止
-		  "no-var": "error",
-		  //早期リターンさせる
-		  "no-else-return": "error",
-		  //厳密等価演算子使用させる
-		  "eqeqeq": [
-			"error",
-			"smart"
-		],
-	}
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:svelte/recommended',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'unused-imports'],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+    extraFileExtensions: ['.svelte']
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true
+  },
+  overrides: [
+    {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser'
+      }
+    }
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'prefer-destructuring': [
+      'error',
+      {
+        array: false,
+        object: true
+      }
+    ],
+    //varを禁止
+    'no-var': 'error',
+    //早期リターンさせる
+    'no-else-return': 'error',
+    //厳密等価演算子使用させる
+    eqeqeq: ['error', 'smart']
+  }
 };
